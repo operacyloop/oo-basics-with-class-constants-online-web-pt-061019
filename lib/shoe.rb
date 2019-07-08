a05594969846c75ffa49908422005f05bde36631
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-08 17:41:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
+BRANDS = []
+
+ #  def initialize(brand)
+ #    @brand = brand
+ #  end
+
+  # def brand=(brand)
+  #  @brand = brand 
+  #  BRANDS << brand
+  # end
 
   def cobble
     self.condition = "new"
